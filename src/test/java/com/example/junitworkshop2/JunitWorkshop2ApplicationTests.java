49f848232dd3bf81815592d77bb839071e706b47
Roman Erzhukov <r.erzhukov@iteco-inno.ru>
2024-05-01 11:39:58 +0300

    JunitWorkshop2ApplicationTests переименован в JunitWorkshop2ApplicationIntegrationTest. Суффикс Test для лёгких Unit-тестов, IntegrationTest — для тяжёлых, интеграционных. Тяжёлое должно и выглядеть тяжелее лёгкого.

--- src/test/java/com/example/junitworkshop2/JunitWorkshop2ApplicationTests.java
+++ src/test/java/com/example/junitworkshop2/JunitWorkshop2ApplicationTests.java
@@ -1,16 +1,11 @@
 package com.example.junitworkshop2;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JunitWorkshop2ApplicationTests {
-
     @Test
     void contextLoads() {
-        Assertions.fail();
     }
-
 }
