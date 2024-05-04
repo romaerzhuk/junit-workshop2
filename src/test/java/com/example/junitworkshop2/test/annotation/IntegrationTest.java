c866e0cdeb02b0fd4bf44b9370c8119191e91b24
Roman Erzhukov <r.erzhukov@iteco-inno.ru>
2024-05-04 12:55:54 +0300

    Добавлен ActiveProfiles("test"). EmployeeSpecificationsIntegrationTest всё ещё падает: employee.id=null

--- src/test/java/com/example/junitworkshop2/test/annotation/IntegrationTest.java
+++ src/test/java/com/example/junitworkshop2/test/annotation/IntegrationTest.java
@@ -5,6 +5,7 @@ import com.example.junitworkshop2.test.init.PostgresTestContainer;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.lang.annotation.ElementType;
@@ -21,6 +22,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @ExtendWith(UidExtension.class)
 @ContextConfiguration(initializers = PostgresTestContainer.Initializer.class)
 public @interface IntegrationTest {
